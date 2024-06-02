683ab666591aabc1aaf7e4a253c1fe0d917cf269
Fabian Salazar <fabianxsalazar@gmail.com>
2024-06-02 16:53:23 -0700

    updated

--- api/index.js
+++ api/index.js
@@ -5,20 +5,14 @@ require('dotenv').config(); // Load environment variables from .env file
 
 // Initialize express app
 const app = express();
+
 const PORT = 3000;
 console.log(`process.env.NODE_ENV==='development' : ${process.env.NODE_ENV==='development'}`)
 /**
  * Middleware to set headers for CORS (Cross-Origin Resource Sharing)
  * Allows requests from specified origin
  */
-app.use((req, res, next) => {
-    console.log(process.env.NODE_ENV)
-    // if(process.env.NODE_ENV==='development'){
-    //     console.log(`NODE_ENV = ${process.env.NODE_ENV}`)
-    //     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5500'); // Uncomment for local development
-    // } else {
-    //     res.setHeader('Access-Control-Allow-Origin', 'https://fsalazar88.github.io'); // Replace with your front-end origin if different
-    // }
+app.use((req, res, next) => {    
     res.setHeader('Access-Control-Allow-Origin', process.env.NODE_ENV==='development' ? 'http://localhost:5500' : 'https://fsalazar88.github.io'); // Uncomment for local development
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Allow GET and POST methods
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allow specific headers
