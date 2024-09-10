189ed07e3a658e52bd6878df61383311c78ef341
Fabian Salazar <fabianxsalazar@gmail.com>
2024-09-09 21:35:30 -0700

    updated comments

--- js/index.js
+++ js/index.js
@@ -6,6 +6,7 @@
  *   - Contact form submission and error handling
  */
 
+
 document.addEventListener('DOMContentLoaded', () => {
     
     // ------------------------------------------------------------------------
